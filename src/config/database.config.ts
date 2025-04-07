0607edba630b73caedb224de4e8d359d06cfc633
arodoo <haro.em@hotmail.com>
2025-04-07 07:48:21 -0600

    memo joining

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   type: process.env.DB_TYPE || 'sqlite',
-  database: process.env.DB_PATH || './data/boopsite.db',
+  database: process.env.DB_PATH || './data/poetry-back.db',
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true, // Set to false in production
   logging: false,
